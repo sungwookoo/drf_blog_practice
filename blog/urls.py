dafd6e93b21650e04cecad59cc26365596d12272
sungwookoo <sungwookoo.dev@gmail.com>
2022-06-27 07:49:20 +0900

    Added Blog urls

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+﻿
+from django.urls import path
+from blog import views
+
+urlpatterns = [
+    # blog/
+    path('', views.BlogView.as_view()),
+    # path('login/', views.BlogApiView.as_view()),
+]
